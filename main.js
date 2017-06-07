7cca7773a09fe17be4d4cf245d442a3e4312fb38
Gaetz <gaetz@rufflerim.com>
2017-06-07 09:49:08 +0200

    Code refactoring

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const STATE_FREEDOM = 'stateFreedom';
 // Variables
 let canvas, canvasContext;
 let gameWidth, gameHeight;
-let currentState, pressedKey;
+let currentState, oldState, pressedKey;
 let text = [];
 
 /**
@@ -32,6 +32,9 @@ window.onload = function () {
     }, 1000 / FRAME_PER_SECOND);
 }
 
+/**
+ * Detect pressed key and translate it in a correct language
+ */
 function checkKey(e) {
     switch (e.keyCode) {
         case 83: pressedKey = 'S'; break;
@@ -54,14 +57,16 @@ function load() {
     canvasContext = canvas.getContext('2d');
     canvasContextAlign = 'center';
     currentState = STATE_CELL;
-    update();
-    draw();
+    oldState = STATE_CELL;
 }
 
 /**
  * Update loop
  */
 function update() {
+    // Update text content and draw it on screen
+    updateTextAndDraw()
+    // Handle state change
     switch (currentState) {
         case STATE_CELL: stateCell(); break;
         case STATE_MIRROR: stateMirror(); break;
@@ -72,137 +77,143 @@ function update() {
         case STATE_LOCK_1: stateLock1(); break;
         case STATE_FREEDOM: stateFreedom(); break;
     }
+    // Reset when state has change
+    if (oldState != currentState) {
+        reset();
+        oldState = currentState;
+    }
 }
 
-function stateCell() {
+/**
+ * Get text and draw it
+ */
+function updateTextAndDraw() {
     if (text.length == 0) {
-        text.push("You are in a prison cell, and you want to escape. There are ");
-        text.push("some dirty sheets on the bed, a mirror on the wall, and the door ");
-        text.push("is locked from the outside.");
-        text.push("Press S to view Sheets");
-        text.push("Press M to look at the Mirror");
-        text.push("Press L to look at the Lock");
+        getText();
         draw();
     }
+}
+
+/**
+ * Set text in function of current state
+ */
+function getText() {
+    switch (currentState) {
+        case STATE_CELL:
+            text.push("You are in a prison cell, and you want to escape. There are ");
+            text.push("some dirty sheets on the bed, a mirror on the wall, and the door ");
+            text.push("is locked from the outside.");
+            text.push("Press S to view Sheets");
+            text.push("Press M to look at the Mirror");
+            text.push("Press L to look at the Lock");
+            break;
+        case STATE_MIRROR:
+            text.push("The dirty old mirror on the wall seems loose.");
+            text.push("Press T to Take the mirror");
+            text.push("Press R to Return");
+            break;
+        case STATE_SHEETS_0:
+            text.push("You can't believe you sleep in these things. Surely it's ");
+            text.push("time somebody change them. The pleasures of prison life ");
+            text.push("you guess !");
+            text.push("Press R to Return");
+            break;
+        case STATE_LOCK_0:
+            text.push("This is one of those button locks. You have no idea what the ");
+            text.push("combinaison is. You wish you could somehow see where the dirty ");
+            text.push("firgerprints were. Maybe that would help.");
+            text.push("Press R to Return roaming your cell.");
+            break;
+        case STATE_CELL_MIRROR:
+            text.push("You are still in your cell and STILL want to escape. There are ");
+            text.push("some dirty sheets on the bed, a mark where the mirror was,");
+            text.push("and that pesky door is still there, and firmly locked.");
+            text.push("Press S to view Sheets");
+            text.push("Press L to look at the Lock");
+            break;
+        case STATE_SHEETS_1:
+            text.push("Holding a mirror in your hand doesn't make the sheets look ");
+            text.push("any better.");
+            text.push("Press R to Return");
+            break;
+        case STATE_LOCK_1:
+            text.push("You carefully but your mirror through the bars, and turn it around ");
+            text.push("so you can see the lock. You canjust make out fingerprints around ");
+            text.push("the buttons. You press dirty buttons, and hear the click.");
+            text.push("Press O to Open");
+            text.push("Press R to Return");
+            break;
+        case STATE_FREEDOM:
+            text.push("You are FREE !");
+            text.push("Press P to Play again");
+            break;
+    }
+}
+
+
+// States
+
+function stateCell() {
+
     if (pressedKey == 'S') {
         currentState = STATE_SHEETS_0;
-        reset();
     }
     else if (pressedKey == 'M') {
         currentState = STATE_MIRROR;
-        reset();
     }
     else if (pressedKey == 'L') {
         currentState = STATE_LOCK_0;
-        reset();
     }
 }
 
 function stateSheets0() {
-    if (text.length == 0) {
-        text.push("You can't believe you sleep in these things. Surely it's ");
-        text.push("time somebody change them. The pleasures of prison life ");
-        text.push("you guess !");
-        text.push("Press R to Return");
-        draw();
-    }
     if (pressedKey == 'R') {
         currentState = STATE_CELL;
-        reset();
     }
 }
 
 function stateMirror() {
-    if (text.length == 0) {
-        text.push("The dirty old mirror on the wall seems loose.");
-        text.push("Press T to Take the mirror");
-        text.push("Press R to Return");
-        draw();
-    }
     if (pressedKey == 'R') {
         currentState = STATE_CELL;
-        reset();
     }
     if (pressedKey == 'T') {
         currentState = STATE_CELL_MIRROR;
-        reset();
     }
 }
 
 function stateLock0() {
-    if (text.length == 0) {
-        text.push("This is one of those button locks. You have no idea what the ");
-        text.push("combinaison is. You wish you could somehow see where the dirty ");
-        text.push("firgerprints were. Maybe that would help.");
-        text.push("Press R to Return roaming your cell.");
-        draw();
-    }
     if (pressedKey == 'R') {
         currentState = STATE_CELL;
-        reset();
     }
 }
 
 function stateCellMirror() {
-    if (text.length == 0) {
-        text.push("You are still in your cell and STILL want to escape. There are ");
-        text.push("some dirty sheets on the bed, a mark where the mirror was,");
-        text.push("and that pesky door is still there, and firmly locked.");
-        text.push("Press S to view Sheets");
-        text.push("Press L to look at the Lock");
-        draw();
-    }
     if (pressedKey == 'S') {
         currentState = STATE_SHEETS_1;
-        reset();
     }
     else if (pressedKey == 'L') {
         currentState = STATE_LOCK_1;
-        reset();
     }
 }
 
 function stateSheets1() {
-    if (text.length == 0) {
-        text.push("Holding a mirror in your hand doesn't make the sheets look ");
-        text.push("any better.");
-        text.push("Press R to Return");
-        draw();
-    }
     if (pressedKey == 'R') {
         currentState = STATE_CELL_MIRROR;
-        reset();
     }
 }
 
 function stateLock1() {
-    if (text.length == 0) {
-        text.push("You carefully but your mirror through the bars, and turn it around ");
-        text.push("so you can see the lock. You canjust make out fingerprints around ");
-        text.push("the buttons. You press dirty buttons, and hear the click.");
-        text.push("Press O to Open");
-        text.push("Press R to Return");
-        draw();
-    }
     if (pressedKey == 'O') {
         currentState = STATE_FREEDOM;
-        reset();
     }
     else if (pressedKey == 'R') {
         currentState = STATE_CELL_MIRROR;
-        reset();
     }
 }
 
 function stateFreedom() {
-    if (text.length == 0) {
-        text.push("You are FREE !");
-        text.push("Press P to Play again");
-        draw();
-    }
     if (pressedKey == 'P') {
         currentState = STATE_CELL;
-        reset();
     }
 }
 
